f0f979be4c04d682d316bd3c76facd7695607473
JapaneseRizz <agehafrom15@gmail.com>
2026-07-26 23:47:48 +0900

    Update 01_basic.sql

--- sql/01_basic.sql
+++ sql/01_basic.sql
@@ -3,33 +3,13 @@
 -- 全体の離職状況を確認する
 -- ============================================
 
-
--- 1. 総社員数
-SELECT
-    COUNT(*) AS total_employees
-FROM
-    `sql-project-459910.IBM_HR_ANALYTICS.risyoku_kaggle`;
-
-
--- 2. 離職状況別の社員数
-SELECT
-    Attrition,
-    COUNT(*) AS employee_count
-FROM
-    `sql-project-459910.IBM_HR_ANALYTICS.risyoku_kaggle`
-GROUP BY
-    Attrition
-ORDER BY
-    employee_count DESC;
-
-
--- 3. 全体離職率
 SELECT
     COUNT(*) AS total_employees,
-    COUNTIF(Attrition = true) AS attrition_count,
+    COUNTIF(Attrition = FALSE) AS active_employees,
+    COUNTIF(Attrition = TRUE) AS attrition_count,
     ROUND(
         SAFE_DIVIDE(
-            COUNTIF(Attrition = true),
+            COUNTIF(Attrition = TRUE),
             COUNT(*)
         ) * 100,
         2
